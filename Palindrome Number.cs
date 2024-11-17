cad977965f16b531e3c2911c823fa501de799fe7
Matheus torrico <matheus-torrico@outlook.com>
2024-11-17 23:35:50 +0100

    Add missing solution

--- Palindrome Number.cs
+++ Palindrome Number.cs
@@ -8,5 +8,28 @@ namespace LeetCode
 {
     internal class Palindrome_Number
     {
+        public bool IsPalindrome(int x)
+        {
+            // Negative numbers are not palindromes
+            if (x < 0) return false;
+
+            // Convert the number to a string
+            string str = x.ToString();
+
+            // Use two pointers to check for palindrome
+            int left = 0, right = str.Length - 1;
+
+            while (left < right)
+            {
+                if (str[left] != str[right])
+                {
+                    return false; // Not a palindrome
+                }
+                left++;
+                right--;
+            }
+
+            return true; // It's a palindrome
+        }
     }
 }
