0ac37c2c205d7fd559e7eff9c63accdef57a9fae
Matheus torrico <matheus-torrico@outlook.com>
2024-11-21 23:31:53 +0100

    new attempt

--- Longest Common Prefix.cs
+++ Longest Common Prefix.cs
@@ -8,5 +8,12 @@ namespace LeetCode
 {
     internal class Longest_Common_Prefix
     {
+        ///<summary>
+        /// Write a function to find the longest common prefix string amongst an array of strings. 
+        /// If there is no common prefix, return an empty string "".
+        /// 
+        /// 
+        /// 
+        /// </summary>-->
     }
 }
