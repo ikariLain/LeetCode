ea96eb1d97b07ba4f5c834a32667df80205f359b
ikariLain <matheus-torrico@outlook.com>
2024-11-22 23:25:18 +0100

    Add 8ms Solution

--- Longest Common Prefix.cs
+++ Longest Common Prefix.cs
@@ -15,5 +15,37 @@ namespace LeetCode
         /// 
         /// 
         /// </summary>-->
+        
+            public string LongestCommonPrefix(string[] strs)
+            {
+                // Edge case: if the array is empty, return an empty string
+                if (strs == null || strs.Length == 0)
+                {
+                    return "";
+                }
+
+                // Start with the first string as the assumed prefix
+                string prefix = strs[0];
+
+                // Loop through the array, updating the prefix
+                for (int i = 1; i < strs.Length; i++)
+                {
+                    // Reduce the prefix until it matches the beginning of strs[i]
+                    while (strs[i].IndexOf(prefix) != 0)
+                    {
+                        prefix = prefix.Substring(0, prefix.Length - 1);
+                        // If prefix becomes empty, return empty string
+                        if (prefix == "")
+                        {
+                            return "";
+                        }
+                    }
+                }
+
+                return prefix;
+            }
+            // Runtime: 8 ms 
+
+
     }
 }
